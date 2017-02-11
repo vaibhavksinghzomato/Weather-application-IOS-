0f55d2c1a63264e31ce43f216d61fef37f4b8291
Vaibhav Kumar Singh <vaibhav@vaibhavs-macbook-air.local>
2017-02-11 17:08:47 +0530

    Initial Commit

---
+++ Downloading Web Content DemoTests/Downloading_Web_Content_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Downloading_Web_Content_DemoTests.swift
+//  Downloading Web Content DemoTests
+//
+//  Created by Vaibhav Kumar Singh on 11/02/17.
+//  Copyright © 2017 Vaibhav Kumar Singh. All rights reserved.
+//
+
+import XCTest
+@testable import Downloading_Web_Content_Demo
+
+class Downloading_Web_Content_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
